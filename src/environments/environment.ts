5a85e95461ee755c08845ffb7a23e45b6487a2d3
Joel <joelts159@gmail.com>
2021-07-25 17:12:14 -0500

    arreglo de errores de environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,13 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const URL_API = 'http://siga_backend.test:8080/v1/';
-export const URL = 'http://siga_backend.test:8080/';
-export const WEB = 'http://siga_backend.test:8080/';
-
+export const URL_API = 'http://siga_backend.test/v1/';
+export const URL = 'http://siga_backend.test/';
+export const WEB = 'http://127.0.0.1:8000/';
 
 export const environment = {
     production: false,
-    STORAGE_URL: URL+'storage/',
+    STORAGE_URL: 'http://siga_backend.test/storage/',
     WEB,
     API_URL_AUTHENTICATION: URL_API + 'authentication/',
     API_URL_APP: URL_API + 'app/',
@@ -20,7 +19,7 @@ export const environment = {
     API_URL_COMMUNITY: URL_API + 'community/',
     SYSTEM_ID: 1,
     CLIENT_ID: '1',
-    CLIENT_SECRET: 'hQPpqSQNRZATRdAtPoQkVlzyoXRy2GtXEWrPa0bl',
+    CLIENT_SECRET: 'H7WJBsRVoD1ltxr7LmVoXCWdWQz3IbhN0H842g2D',
     GRANT_TYPE: 'password',
 
     SITE_KEY: 'QpKtUkaJ40HNd1Di74B0pKC6asgKzAxuTx69L9Nb',
